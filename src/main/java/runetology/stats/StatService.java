cc777e6d5216022e5ea0326e5b6213aa1917a537
Neto test <sysl86@mail.ru>
2023-06-17 16:26:55 +0400

    Initial commit

---
+++ src/main/java/runetology/stats/StatService.java
@@ -0,0 +1,36 @@
+package runetology.stats;
+public class StatService {
+    public long sum (long [] sales){
+        long totalSale = 0;
+        for (long sale : sales){
+            totalSale += sale;
+        }
+        return totalSale;
+    }
+    public long average (long [] sales){
+
+                return sum(sales) /sales.length;
+    }
+    public int monthMaximumSale(long[] sales){
+        int monthMaximum = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales [i] >= sales [monthMaximum]){
+                monthMaximum = i;
+
+            }
+            
+        }
+        return monthMaximum +1;
+    }
+    public int monthMinimumSale(long[] sales){
+        int monthMinimum = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales [i] <= sales [monthMinimum]){
+                monthMinimum = i;
+
+            }
+
+        }
+        return monthMinimum +1;
+    }
+}
---
+++ src/test/java/runetology/stats/StatServiceTest.java
@@ -0,0 +1,47 @@
+package runetology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatServiceTest {
+    @Test
+    public void testSum() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAverage() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.average(sales);
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testMaximumSale() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.monthMaximumSale(sales);
+        long expected = 8;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMinimumSale() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.monthMinimumSale(sales);
+        long expected = 9;
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
