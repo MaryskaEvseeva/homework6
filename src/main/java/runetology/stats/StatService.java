dbd105eae430c776534838307d6484f4d52f8c24
Neto test <sysl86@mail.ru>
2023-06-21 21:09:20 +0400

    second commit

--- src/main/java/runetology/stats/StatService.java
+++ src/main/java/runetology/stats/StatService.java
@@ -1,53 +1,61 @@
 package runetology.stats;
+
 public class StatService {
-    public long sum (long [] sales){
+    public long sum(long[] sales) {
         long totalSale = 0;
-        for (long sale : sales){
+        for (long sale : sales) {
             totalSale += sale;
         }
         return totalSale;
     }
-    public long average (long [] sales){
 
-                return sum(sales) /sales.length;
+    public long average(long[] sales) {
+
+        return sum(sales) / sales.length;
     }
-    public int monthMaximumSale(long[] sales){
+
+    public int monthMaximumSale(long[] sales) {
         int monthMaximum = 0;
         for (int i = 0; i < sales.length; i++) {
-            if (sales [i] >= sales [monthMaximum]){
+            if (sales[i] >= sales[monthMaximum]) {
                 monthMaximum = i;
 
             }
-            
+
         }
-        return monthMaximum +1;
+        return monthMaximum + 1;
     }
-    public int monthMinimumSale(long[] sales){
+
+    public int monthMinimumSale(long[] sales) {
         int monthMinimum = 0;
         for (int i = 0; i < sales.length; i++) {
-            if (sales [i] <= sales [monthMinimum]){
+            if (sales[i] <= sales[monthMinimum]) {
                 monthMinimum = i;
 
             }
 
         }
-        return monthMinimum +1;
+        return monthMinimum + 1;
     }
-    public int salesBellowAverage (long[] sales){
+
+    public int salesBellowAverage(long[] sales) {
         int couter = 0;
-        for (long sale :sales){
-            if (sale < average(sales)){
-                couter ++;
+        long averageSale = average(sales);
+        for (long sale : sales) {
+            if (sale < average(sales)) {
+                couter++;
 
             }
         }
         return couter;
     }
-    public int salesAboveAverage (long[] sales){
+
+    public int salesAboveAverage(long[] sales) {
         int couter = 0;
-        for (long sale :sales){
-            if (sale > average(sales)){
-                couter ++;
+        long averageSale = average(sales);
+        for (long sale : sales) {
+            if (sale > average(sales)) {
+                couter++;
 
             }
         }
